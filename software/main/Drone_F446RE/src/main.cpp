f8b0abe01b701a842ff5c10d4a0151e12c701345
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2024-10-27 11:25:19 +0900

    done debug2

--- software/main/Drone_F446RE/src/main.cpp
+++ software/main/Drone_F446RE/src/main.cpp
@@ -1,24 +1,26 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 #include "gyro.h"
 
-#define MOTOR_1 PB_5
-#define MOTOR_2 PB_7
-#define MOTOR_3 PA_0
-#define MOTOR_4 PA_1
-#define LED_1 PB_13
-#define LED_2 PB_15
-#define LED_3 PA_5
-#define LED_4 PA_7
+#define MOTOR_1 PB5
+#define MOTOR_2 PB7
+#define MOTOR_3 PA0
+#define MOTOR_4 PA1
+#define LED_1 PB13
+#define LED_2 PB15
+#define LED_3 PA5
+#define LED_4 PA7
 
-// Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
-// GYRO gyro(&bno);
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire);
+GYRO gyro(&bno);
 
-HardwareSerial serial(PB_6, NC);
+SoftwareSerial MySerial(PB4, PB3);
+
 
 void setup() {
-      serial.begin(9600);
-      serial.printf("START");
+      MySerial.begin(115200);
+      MySerial.println("START");
       pinMode(MOTOR_1, OUTPUT);
       pinMode(MOTOR_2, OUTPUT);
       pinMode(MOTOR_3, OUTPUT);
@@ -28,16 +30,22 @@ void setup() {
       pinMode(LED_3, OUTPUT);
       pinMode(LED_4, OUTPUT);
 
-      digitalWrite(LED_1, LOW);
-      digitalWrite(LED_2, LOW);
-      digitalWrite(LED_3, LOW);
-      digitalWrite(LED_4, LOW);
+      digitalWrite(LED_1, HIGH);
+      digitalWrite(LED_2, HIGH);
+      digitalWrite(LED_3, HIGH);
+      digitalWrite(LED_4, HIGH);
 
       Wire.setSDA(PC_7);
       Wire.setSCL(PC_6);
       Wire.begin();
-      // gyro.init();
-      // gyro.setOffset();
+      gyro.init();
+      gyro.setOffset();
+      delay(100);
+
+      digitalWrite(LED_1, LOW);
+      digitalWrite(LED_2, LOW);
+      digitalWrite(LED_3, LOW);
+      digitalWrite(LED_4, LOW);
 }
 void loop() {
       // analogWrite(MOTOR_1, 200);
@@ -45,10 +53,18 @@ void loop() {
       // analogWrite(MOTOR_3, 200);
       // analogWrite(MOTOR_4, 200);
       // delay(1000);
-      // int yaw = gyro.read();
-      // serial.printf("%d¥n", yaw);
-      digitalWrite(LED_1, HIGH);
-      digitalWrite(LED_2, HIGH);
-      digitalWrite(LED_3, HIGH);
-      digitalWrite(LED_4, HIGH);
+      gyro.read();
+      int yaw = gyro.deg;
+      MySerial.printf("%d\n", yaw);
+      // if (yaw < 0) {
+      //       digitalWrite(LED_1, HIGH);
+      //       digitalWrite(LED_2, HIGH);
+      //       digitalWrite(LED_3, HIGH);
+      //       digitalWrite(LED_4, HIGH);
+      // } else {
+      //       digitalWrite(LED_1, LOW);
+      //       digitalWrite(LED_2, LOW);
+      //       digitalWrite(LED_3, LOW);
+      //       digitalWrite(LED_4, LOW);
+      // }
 }
